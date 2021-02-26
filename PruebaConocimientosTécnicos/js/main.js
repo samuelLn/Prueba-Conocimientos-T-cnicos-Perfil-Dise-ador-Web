c800eb67edb161ce831a8208a8662f5e7886647b
samuelLn <44329599+samuelLn@users.noreply.github.com>
2021-02-26 03:34:48 -0300

    Add files via upload

---
+++ PruebaConocimientosTécnicos/js/main.js
@@ -0,0 +1,221 @@
+console.log("Hello world!");
+
+/*
+$(document).ready(function(){
+	// Menu Responsive
+	$(".infoBuscador i").click(function(e){
+		e.preventDefault();
+		if($(".infoBuscador i").attr('class') == 'fas fa-angle-right'){
+			$(".infoBuscador i").removeClass('fas fa-angle-right').addClass('fas fa-arrows-alt-h');
+			
+		} else {
+			$(".infoBuscador i").removeClass('fas fa-arrows-alt-h').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+	
+			
+
+
+});*/
+
+
+
+$(document).ready(function(){
+	$(".prueba i").click(function(e){
+		e.preventDefault();
+		if($(".prueba i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+}
+
+
+);
+
+
+
+$(document).ready(function(){
+	$(".prueba1 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba1 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba1 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba1 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+
+$(document).ready(function(){
+	$(".prueba2 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba2 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba2 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba2 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+	$(".prueba3 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba3 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba3 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba3 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+
+$(document).ready(function(){
+	$(".prueba4 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba4 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba4 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba4 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+$(document).ready(function(){
+	$(".prueba5 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba5 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba5 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba5 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+
+$(document).ready(function(){
+	$(".prueba6 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba6 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba6 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba6 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+$(document).ready(function(){
+	$(".prueba7 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba7 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba7 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba7 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+$(document).ready(function(){
+	$(".prueba8 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba8 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba8 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba8 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+$(document).ready(function(){
+	$(".prueba9 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba9 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba9 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba9 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+
+$(document).ready(function(){
+	$(".prueba10 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba10 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba10 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba10 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+$(document).ready(function(){
+	$(".prueba11 i").click(function(e){
+		e.preventDefault();
+		if($(".prueba11 i").attr('class') == 'fas fa-angle-right'){
+			$(".prueba11 i").removeClass('fas fa-angle-right').addClass('fas fa-angle-down');
+			
+		} else {
+			$(".prueba11 i").removeClass('fas fa-angle-down').addClass('fas fa-angle-right');
+			
+		}
+	});
+
+});
+
+
+
+
+
